5944bed25cc6184b5e2b3d2c54cda80582741864
Joseph Catera <zhouyaoji@yahoo.com>
2015-10-05 23:00:28 -0700

    Changing the port.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 express = require('express'),
 app = express();
+app.set('port', (process.env PORT || 5000));
 http = require('http');
 httpServer = http.Server(app);
 
@@ -8,4 +9,6 @@ app.use(express.static(__dirname+'/static'));
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/index.html');
 });
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running on port', app.get('port'));
+});
